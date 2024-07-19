2bc825743f878b1c75b5b150ef8290b6791382db
Joeeazy <joeeazy34@gmail.com>
2024-07-19 05:35:45 +0300

    framer motion animations added

--- src/components/WhereToBuy.jsx
+++ src/components/WhereToBuy.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import worldMap from "../../public/background/world-map.png";
+import { motion } from "framer-motion";
 
 export default function WhereToBuy() {
   return (
@@ -7,11 +8,28 @@ export default function WhereToBuy() {
       <div className="grid grid-cols-1 sm:grid-cols-3 gap-8 place-items-center">
         {/* form section */}
         <div className="space-y-8">
-          <h1 className="text-4xl font-bold text-darkGrey font-serif">
+          <motion.h1
+            initial={{ opacity: 0, y: 100 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{
+              delay: 0.2,
+              type: "spring",
+              stiffness: 100,
+              damping: 10,
+            }}
+            className="text-4xl font-bold text-darkGrey font-serif"
+          >
             Buy Our Products from Anywhere
-          </h1>
+          </motion.h1>
 
-          <div className="flex items-center gap-4">
+          <motion.div
+            initial={{ opacity: 0, y: 100 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{
+              delay: 0.4,
+            }}
+            className="flex items-center gap-4"
+          >
             {/* input */}
             <input
               type="text"
@@ -23,8 +41,15 @@ export default function WhereToBuy() {
               placeholder="email"
               className="input-style w-full "
             />
-          </div>
-          <div className="flex items-center gap-4">
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0, y: 100 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{
+              delay: 0.6,
+            }}
+            className="flex items-center gap-4"
+          >
             {/* input */}
             <input
               type="text"
@@ -36,12 +61,29 @@ export default function WhereToBuy() {
               placeholder="ZipCode"
               className="input-style w-full "
             />
-          </div>
-          <button className="primary-btn w-full">Order Now</button>
+          </motion.div>
+          <motion.button
+            initial={{ opacity: 0, y: 100 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{
+              delay: 0.8,
+            }}
+            className="primary-btn w-full"
+          >
+            Order Now
+          </motion.button>
         </div>
         {/* world map section */}
         <div className="col-span-2">
-          <img
+          <motion.img
+            initial={{ opacity: 0, scale: 0.5 }}
+            whileInView={{ opacity: 1, scale: 1 }}
+            transition={{
+              delay: 1,
+              type: "spring",
+              stiffness: 100,
+              damping: 10,
+            }}
             src={worldMap}
             alt="worldmap"
             className="w-full sm:w-[500px] mx-auto"
