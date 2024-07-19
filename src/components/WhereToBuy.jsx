40038d38ed4329f3ef813cfee868acbe2390ef09
Joeeazy <joeeazy34@gmail.com>
2024-07-19 05:26:37 +0300

    input tags

--- src/components/WhereToBuy.jsx
+++ src/components/WhereToBuy.jsx
@@ -6,10 +6,38 @@ export default function WhereToBuy() {
     <div className="container my-36">
       <div className="grid grid-cols-1 sm:grid-cols-3 gap-8 place-items-center">
         {/* form section */}
-        <div>
-          <h1>Buy Our Products from Anywhere</h1>
+        <div className="space-y-8">
+          <h1 className="text-4xl font-bold text-darkGrey font-serif">
+            Buy Our Products from Anywhere
+          </h1>
 
-          <div>{/* input */}</div>
+          <div className="flex items-center gap-4">
+            {/* input */}
+            <input
+              type="text"
+              placeholder="name"
+              className="input-style w-full lg:w-[150px]"
+            />
+            <input
+              type="email"
+              placeholder="email"
+              className="input-style w-full "
+            />
+          </div>
+          <div className="flex items-center gap-4">
+            {/* input */}
+            <input
+              type="text"
+              placeholder="Country"
+              className="input-style w-full lg:w-[150px]"
+            />
+            <input
+              type="text"
+              placeholder="ZipCode"
+              className="input-style w-full "
+            />
+          </div>
+          <button className="primary-btn w-full">Order Now</button>
         </div>
         {/* world map section */}
         <div className="col-span-2">
